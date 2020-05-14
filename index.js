f145af03d4f3d9bf9add78a251a678d329b70e9e
nzambello <nzambello@protonmail.com>
2020-05-14 19:05:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Parser = require("rss-parser");
+const parser = new Parser();
+
+const rssFeeds = {
+  ansa: [
+    "https://www.ansa.it/sito/ansait_rss.xml",
+    "https://www.ansa.it/sito/notizie/cronaca/cronaca_rss.xml",
+    "https://www.ansa.it/sito/notizie/politica/politica_rss.xml",
+    "https://www.ansa.it/sito/notizie/topnews/topnews_rss.xml",
+    "https://www.ansa.it/emiliaromagna/notizie/emiliaromagna_rss.xml",
+    "https://www.ansa.it/liguria/notizie/liguria_rss.xml"
+  ]
+};
+
+exports.handler = async function() {
+  return {
+    feeds: await Promise.all(
+      Object.keys(rssFeeds).map(async key => ({
+        [key]: await Promise.all(
+          rssFeeds[key].map(async feedURL => {
+            let feed = await parser.parseURL(feedURL);
+            return feed.items.map(item => ({
+              ...item,
+              feedURL,
+              feedTitle: feed.title
+            }));
+          })
+        )
+      }))
+    )
+  };
+};

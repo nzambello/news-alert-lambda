2521d55e720787e4d9431a1ac35eae5ff00b4a43
nzambello <nzambello@protonmail.com>
2020-05-15 19:20:38 +0200

    added repubblica rss and uniq for rss results

--- functions/news-alert-feed/news-alert-feed.js
+++ functions/news-alert-feed/news-alert-feed.js
@@ -1,14 +1,23 @@
 const Parser = require('rss-parser')
+const uniqBy = require('lodash.uniqby')
 const parser = new Parser()
 
 const rssFeeds = {
-  ansa: [
+  ANSA: [
     'https://www.ansa.it/sito/ansait_rss.xml',
     'https://www.ansa.it/sito/notizie/cronaca/cronaca_rss.xml',
     'https://www.ansa.it/sito/notizie/politica/politica_rss.xml',
     'https://www.ansa.it/sito/notizie/topnews/topnews_rss.xml',
     'https://www.ansa.it/emiliaromagna/notizie/emiliaromagna_rss.xml',
     'https://www.ansa.it/liguria/notizie/liguria_rss.xml'
+  ],
+  Repubblica: [
+    'http://www.repubblica.it/rss/homepage/rss2.0.xml',
+    'http://www.repubblica.it/rss/esteri/rss2.0.xml',
+    'http://www.repubblica.it/rss/economia/rss2.0.xml',
+    'http://www.repubblica.it/rss/esteri/rss2.0.xml',
+    'http://www.repubblica.it/rss/solidarieta/rss2.0.xml',
+    'http://www.repubblica.it/rss/ambiente/rss2.0.xml'
   ]
 }
 
@@ -16,18 +25,21 @@ exports.handler = async function () {
   const feed = await Promise.all(
     Object.keys(rssFeeds).map(async (key) => ({
       title: key,
-      data: (
-        await Promise.all(
-          rssFeeds[key].map(async (feedURL) => {
-            let feed = await parser.parseURL(feedURL)
-            return feed.items.map((item) => ({
-              ...item,
-              feedURL,
-              feedTitle: feed.title
-            }))
-          })
-        )
-      ).flat()
+      data: uniqBy(
+        (
+          await Promise.all(
+            rssFeeds[key].map(async (feedURL) => {
+              let feed = await parser.parseURL(feedURL)
+              return feed.items.map((item) => ({
+                ...item,
+                feedURL,
+                feedTitle: feed.title
+              }))
+            })
+          )
+        ).flat(),
+        'link'
+      )
     }))
   )
 
